b40ffc8fe37bb9a0441ea713ab9aebe945a6735f
Grootoportunities <tapir1337@mail.ru>
2023-10-16 21:07:54 +0300

    Logo Update

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import { Logo } from "../../components/logo/Logo";
 import { Menu } from "../../components/menu/Menu";
+import logoImage from "../../assets/images/Logo__4_-removebg-preview.png";
 
 const items = ["Home", "Projects", "About", "Content"];
 
 export const Header = () => {
   return (
     <StyledHeader>
-      <Logo />
+      {/*<Logo />*/}
+      <LogoImage />
       <Menu menuItems={items} />
     </StyledHeader>
   );
@@ -18,4 +20,12 @@ const StyledHeader = styled.header`
   background-color: #a6bcfa;
   display: flex;
   justify-content: space-between;
+`;
+
+const LogoImage = styled.img.attrs({
+  src: logoImage,
+})`
+  object-fit: cover;
+  //height: 100px;
+  width: 80px;
 `;

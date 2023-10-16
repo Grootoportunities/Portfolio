72c4a5a16a3d7b7f8d26f041a54d200cbc7d638a
Grootoportunities <tapir1337@mail.ru>
2023-10-16 21:07:54 +0300

    Logo Update

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Icon } from "../icon/Icon";
+import styled from "styled-components";
+import logoImage from "../../assets/images/Logo__4_-removebg-preview.png";
 
 export const Logo = () => {
   return (
     <div>
       <a>
-        <Icon iconId="edit" />
+        <LogoImage />
       </a>
     </div>
   );
 };
+
+const LogoImage = styled.img.attrs({
+  src: logoImage,
+})`
+  object-fit: cover;
+  width: 80px;
+`;
